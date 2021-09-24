5d792706bfed33f2b44e401edd1a25e5e10e0a57
Natalia Fedorova <nataliafedorova1989@gmail.com>
2021-09-24 21:22:06 +0200

    add map, edit styles, add media quieries

--- src/components/PackageModal.jsx
+++ src/components/PackageModal.jsx
@@ -1,21 +1,52 @@
+// NPM packages
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 // Project files
-import PackageLogo from "../assets/pictures/package.png"
+import PackageLogo from "../assets/pictures/package.png";
+import DateTime from "./DateTime.jsx";
+import StatusConverter from "./StatusConverter";
 
 export default function PackageModal({ item }) {
-    const {sender, eta, location_name, parcel_id, status} = item;
-    return(
-        <div id="modal-popup">
-            <div className="upper-part">
-                <h3>Parcel information</h3>
-                <div className="package-logo">
-                    <img src={PackageLogo} alt="an illustration of a brown box in a yellow circle"/>
-                </div>
-            </div>
-            <p>From: {sender}</p>
-            <p>Parcel ID: {parcel_id}</p>
-            <p>Estimated arrival: {eta}</p>
-            <p>Order status: {status}</p>
-            <p>Pickup location: {location_name}</p>
+  const {
+    sender,
+    eta,
+    location_name,
+    parcel_id,
+    status,
+    location_coordinate_latitude,
+    location_coordinate_longitude,
+  } = item;
+  return (
+    <div id="modal-popup">
+      <div className="upper-part">
+        <h3>Parcel information</h3>
+        <div className="package-logo">
+          <img src={PackageLogo} alt="a brown box in a circle" />
         </div>
-    )
+      </div>
+      <p>From: {sender}</p>
+      <p>Parcel ID: {parcel_id}</p>
+      <DateTime dateTimeString={eta} />
+      <div id="status">
+        <p>Status:</p>
+        <StatusConverter orderStatus={status} />
+      </div>
+      <p>Pickup location: {location_name}</p>
+      <MapContainer
+        center={[location_coordinate_latitude, location_coordinate_longitude]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker
+          position={[
+            location_coordinate_latitude,
+            location_coordinate_longitude,
+          ]}
+        ></Marker>
+      </MapContainer>
+    </div>
+  );
 }
--- src/sections/Track.jsx
+++ src/sections/Track.jsx
@@ -1,48 +1,48 @@
 // NPM Packages
 import { useEffect, useState } from "react";
 // Project files
-import TrackPhoto from "../assets/pictures/track.jpg"
+import TrackPhoto from "../assets/pictures/track.jpg";
 import PackageItem from "../components/PackageItem";
 import PackageModal from "../components/PackageModal";
 
-export default function Track({ setModal }){
+export default function Track({ setModal }) {
   // Local State
   const [status, setStatus] = useState(0);
   const [parcels, setParcels] = useState([]);
-  // Constants 
-  const API_URL = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840"; 
+  // Constants
+  const API_URL = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840";
   // List of Package previews
   const PackagesList = parcels.map((item) => (
-      <PackageItem key={item.id} item={item} onClick={()=> onPackage(item)}/>
-  ))
+    <PackageItem key={item.id} item={item} onClick={() => onPackage(item)} />
+  ));
   // Custom hook
-  useEffect(()=> {
+  useEffect(() => {
     fetch(API_URL)
-    .then((response)=>response.json())
-    .then(onFetchSuccess)
-    .catch(onFetchFail); 
+      .then((response) => response.json())
+      .then(onFetchSuccess)
+      .catch(onFetchFail);
   }, [setParcels, setStatus]);
   // Methods
   function onFetchSuccess(json) {
     setParcels(json);
     setStatus(2);
   }
-  function onFetchFail(error){
+  function onFetchFail(error) {
     console.error("There was an error", error);
     setStatus(1);
   }
   function onPackage(item) {
-    setModal(<PackageModal item={item}/>);
+    setModal(<PackageModal item={item} />);
   }
-    return(
-        <section id="track">
-          <div className="track-photo">
-            <img src={TrackPhoto} alt="an illustration of a hand holding a mobile phone"/>
-          </div>
-          <h1>Track my parcel</h1>
-          {status === 0 && <p>Loading</p>}
-          {status === 1 && <p>Error</p>}
-          {status === 2 && PackagesList}
-        </section>
-    )
+  return (
+    <section id="track">
+      <div className="track-photo">
+        <img src={TrackPhoto} alt="a hand with a phone" />
+      </div>
+      <h1>Track my parcel</h1>
+      {status === 0 && <p>Loading</p>}
+      {status === 1 && <p>Error</p>}
+      {status === 2 && PackagesList}
+    </section>
+  );
 }
